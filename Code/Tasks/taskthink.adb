ea419aabe7b573ed49fae0c69bbf4b985ac0a99b
NicolasOlsen <nicolas.t.f.olsen@gmail.com>
2024-10-28 13:41:00 +0100

    changed so values are only needed to get once from brain

--- Code/Tasks/taskthink.adb
+++ Code/Tasks/taskthink.adb
@@ -6,20 +6,25 @@ package body TaskThink is
 
   task body think is
    myClock : Time;
+   sensor1 : Distance_cm;
+   sensor2 : Distance_cm;
    minDist : constant Distance_cm := 10;
    midDist : constant Distance_cm := 20;
    begin
       loop
          myClock := Clock;
 
+         sensor1 := Brain.GetMeasurementSensor1;
+         sensor2 := Brain.GetMeasurementSensor2;
+
          --make a decision (could be wrapped nicely in a procedure)
-         if Brain.GetMeasurementSensor1 < minDist or Brain.GetMeasurementSensor2 < minDist then
+         if sensor1 < minDist or sensor2 < minDist then
             MotorDriver_custom.SetDirection (Backward); --our decision what to do based on the sensor values
-         elsif Brain.GetMeasurementSensor1 > midDist and Brain.GetMeasurementSensor2 > midDist then
+         elsif sensor1 > midDist and sensor2 > midDist then
             MotorDriver_custom.SetDirection (Forward);
-         elsif Brain.GetMeasurementSensor1 < midDist then
+         elsif sensor1 < midDist then
             MotorDriver_custom.SetDirection (Forward);
-         elsif Brain.GetMeasurementSensor2 < midDist then
+         elsif sensor2 < midDist then
             MotorDriver_custom.SetDirection (Forward);
          end if;
 
