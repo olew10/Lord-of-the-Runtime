af8d8e8743e0da75dee1d971dc1c59cb43cf5651
olew10 <ole_russ@hotmail.no>
2024-11-20 09:44:09 +0100

    Respons-time works

--- Code/src/priorities.ads
+++ Code/src/priorities.ads
@@ -2,11 +2,12 @@ with Ada.Real_Time; use Ada.Real_Time;
 package Priorities is
 
    Sense : constant := 1;
-   senseDeadline : constant Time_Span := Milliseconds(130);
+   senseDeadline : constant Time_Span := Milliseconds(140);
+
+   Act : constant := 2;
+   actDeadline : constant Time_Span := Milliseconds(50);
 
    Think : constant := 3;
    thinkDeadline : constant Time_Span := Milliseconds(10);
 
-   Act : constant := 2;
-   actDeadline : constant Time_Span := Milliseconds(40);
 end Priorities;
